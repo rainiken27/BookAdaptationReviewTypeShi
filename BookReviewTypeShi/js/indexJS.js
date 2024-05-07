669ddee8334284216107d8a52dcf22d7c2ca3490
rainiken27 <99723586+rainiken27@users.noreply.github.com>
2024-05-07 08:46:31 +0800

    Delete BookReviewTypeShi/js directory

--- BookReviewTypeShi/js/indexJS.js
+++
@@ -1,9 +0,0 @@
-//makes the login form visible to the user 
-function openLoginForm() {
-	document.getElementById("loginOverlay").style.display="block";
-}
-
-//login form disappears when user clicks on the x button in the corner
-function closeLoginForm() {
-	document.getElementById("loginOverlay").style.display="none";
-}
